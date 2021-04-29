a331c42fdb159ee12771cdf2944a86a5e0012c5a
jacobitkashala <jacobitkashala@gmail.com>
2021-04-29 12:23:02 +0200

    modification

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,19 +1,22 @@
-import { useState} from 'react'
-import React from 'react';
-import './Style.css';
+import React from "react";
+import "./Style.css";
 
 const Cart = (props) => {
   const { name, email, uriImg } = props.dataUser;
-  
+
   return (
-    <div className="bloc--imag col-sm-3"id="" >  
-        <img  src={uriImg} className=""  onClick={props.clickCart} alt="image--profil" />
+    <div className="bloc--imag col-sm-3" id="">
+      <img
+        src={uriImg}
+        className=""
+        onClick={props.clickCart}
+        alt="image--profil"
+      />
       <div className="bloc--info">
         <h2 className="">{name}</h2>
         <h2 className="">{email}</h2>
       </div>
-    </div>)
-  
-  
-  }
+    </div>
+  );
+};
 export default Cart;
